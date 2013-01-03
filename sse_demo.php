900da9a5d38fe23dbbf95f499201ed2c20beaa02
RubenJnl <ruben@ubenj.nl>
2013-01-03 14:49:14 +0100

    Proof of concept
    Basic proof of concept.

---
+++ sse_demo.php
@@ -0,0 +1,10 @@
+﻿<?php
+header('Content-Type: text/event-stream');
+header('Cache-Control: no-cache');
+
+$time = date('r');
+echo "data: The server time is: {$time}\n\n";
+ob_flush();
+flush();
+
+?> 
